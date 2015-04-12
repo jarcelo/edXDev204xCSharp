bd7fe08e3bfaad5f2f9d4cadf82bf89bc7623b13
jarcelo <josepharcelo@gmail.com>
2015-04-12 18:16:45 -0500

    add a succint code from edEx peer

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -49,3 +49,30 @@ namespace Homework2
         }
     }
 }
+
+/*
+class Program
+{
+    static void Main(string[] args)
+    {
+        char[] xo = new char[] { 'X', 'O' };
+
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                // The key is the
+                // (i+j)%2 as an index
+                // and the xo array;
+                // if i+j are even that statement will return with 0
+                // so xo[0] --> X
+                // if odd 
+                // xo[1] --> O
+                Console.Write(xo[(i + j) % 2]);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+*/
