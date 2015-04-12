e1743e6b042e5a9e8166f46885702a391408b4ad
jarcelo <josepharcelo@gmail.com>
2015-04-12 18:09:39 -0500

    add HW2 code

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -10,6 +10,42 @@ namespace Homework2
     {
         static void Main(string[] args)
         {
+            int colCount;
+            for (colCount = 1; colCount <= 8; colCount++)
+            {
+                for (int rowCount = 1; rowCount <= 8; rowCount++)
+                {
+                    bool colIsOdd = !(colCount % 2 == 0);
+                    bool rowIsOdd = !(rowCount%2 == 0);
+
+                    switch (colIsOdd)
+                    {
+                        case true:
+                            if (rowIsOdd)
+                            {
+                                Console.Write("X");
+                            }
+                            else
+                            {
+                                Console.Write("O");
+                            }
+                            break;
+
+                        case false:
+                            if (rowIsOdd)
+                            {
+                                Console.Write("O");
+                            }
+                            else
+                            {
+                                Console.Write("X");
+                            }
+                            break;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
