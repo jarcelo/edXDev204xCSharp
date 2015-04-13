67e7deb33a89b52a22ab6d5cb8de82200678a2ac
jarcelo <josepharcelo@gmail.com>
2015-04-13 15:01:21 -0500

    Add methods to get teacher info and to print it

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -10,6 +10,34 @@ namespace Homework3
     {
         static void Main(string[] args)
         {
+            // Call GetStudentInfor method
+            GetStudentInfo();
+            Console.ReadLine();
+            // Output Student Information
+            //Console.WriteLine();
+            //Console.WriteLine(" Student ID: {0}\n Student Name: {1} {2} \n Birthday: {3}\n Address: {4} {5}\n City: {5}\n State/Province: {8} Country: {10} Zip Code: {9}",
+            //    studentID, firstName, lastName, birthDate, gender, address1, address2, city, stateOrProvince, zipCode, country);
+            //Console.WriteLine();
+
+        }
+
+        // Method that prompts the user to input student information
+        static void GetStudentInfo()
+        {
+            Console.WriteLine("***ENTER STUDENT INFORMATION***");
+            Console.Write("Student ID: ");
+            string studentID = Console.ReadLine();
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+            PrintStudentDetails(studentID, firstName, lastName);
+        }
+        static void PrintStudentDetails(string id, string fName, string lName)
+        {
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("RECORD: {1} {2} has a student number: {0}", id, fName, lName);
+            Console.WriteLine("-------------------------------------------");
         }
     }
 }
