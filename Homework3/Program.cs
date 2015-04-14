bdef0485999085ca5b3ff86d8017aed2ec925581
jarcelo <josepharcelo@gmail.com>
2015-04-13 22:42:07 -0500

    Add method to get course info and print it

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -12,13 +12,9 @@ namespace Homework3
         {
             // Call GetStudentInfor method
             GetStudentInfo();
+            GetTeacherInfo();
             Console.ReadLine();
-            // Output Student Information
-            //Console.WriteLine();
-            //Console.WriteLine(" Student ID: {0}\n Student Name: {1} {2} \n Birthday: {3}\n Address: {4} {5}\n City: {5}\n State/Province: {8} Country: {10} Zip Code: {9}",
-            //    studentID, firstName, lastName, birthDate, gender, address1, address2, city, stateOrProvince, zipCode, country);
-            //Console.WriteLine();
-
+            // Create a method to get information for a teacher, a course, and program, and a degree using a similar method as above
         }
 
         // Method that prompts the user to input student information
@@ -33,10 +29,32 @@ namespace Homework3
             string lastName = Console.ReadLine();
             PrintStudentDetails(studentID, firstName, lastName);
         }
+        // Method that prints student information
         static void PrintStudentDetails(string id, string fName, string lName)
         {
             Console.WriteLine("-------------------------------------------");
             Console.WriteLine("RECORD: {1} {2} has a student number: {0}", id, fName, lName);
+            Console.WriteLine("-------------------------------------------");
+        }
+
+        // Method that prompts the user to input teacher information
+        static void GetTeacherInfo()
+        {
+            Console.WriteLine("***ENTER TEACHER INFORMATION***");
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Course to teach next semester: ");
+            string subject = Console.ReadLine();
+            PrintTeacherInfo(firstName, lastName, subject);
+        }
+
+        static void PrintTeacherInfo(string firstName, string lastName, string subject)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Teacher: {0} {1} will teach: {2}", firstName, lastName, subject);
             Console.WriteLine("-------------------------------------------");
         }
     }
